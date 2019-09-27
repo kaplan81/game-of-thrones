589ab281f79629db5a9ff87b960a5c0dd865f86f
Andres Gesteira <gesteira.pro@gmail.com>
2019-09-27 18:37:55 +0200

    new monorepo + new app

---
+++ projects/game-of-thrones-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'got-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'game-of-thrones-app';
+}
