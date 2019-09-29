6f8b53bf07fda564b41e3ee916f286be9624dcac
Andres Gesteira <gesteira.pro@gmail.com>
2019-09-29 18:37:29 +0200

    config services in library

---
+++ projects/@got/ng-kit/src/lib/services/config-file/config-file.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, take, tap } from 'rxjs/operators';
+import { ErrorService } from '../error/error.service';
+
+export const configFileparams = new HttpParams()
+  .set('configRequest', 'jsonFile')
+  .append(`v`, `${new Date().getTime()}`);
+
+/**
+ * Get config.json
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigFileService {
+  get configs(): any {
+    return this._configs;
+  }
+  set configs(configs: any) {
+    this._configs = configs;
+  }
+  private _configs: any;
+
+  constructor(private errorService: ErrorService, private http: HttpClient) {}
+
+  configFile<T>(): Observable<T> {
+    return this.http.get<T>('config.json', { params: configFileparams }).pipe(
+      tap((configFile: T) => (this.configs = configFile)),
+      take(1),
+      catchError(this.errorService.handleHttpError('loadConfig'))
+    );
+  }
+}
--- projects/@got/ng-kit/src/public-api.ts
+++ projects/@got/ng-kit/src/public-api.ts
@@ -1,3 +1,5 @@
 /*
  * Public API Surface of ng-kit
  */
+export * from './lib/services/config-file/config-file.service';
+export * from './lib/services/error/error.service';
